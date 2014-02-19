4fe43b38c9d44ba49b2a3d715f5efaadfaaf79b1
Filipe Arantes Fernandes <filran@gmail.com>
2014-02-18 21:21:59 -0300

    Implementação do Zoom
    Ainda há bug quando aplica o zoom

--- scripts/plugins/zoom.js
+++ scripts/plugins/zoom.js
@@ -1,54 +1,58 @@
 //PLUGIN ZOOM
 (function($){
                          //"*","1.2","#pmcanvas","width"
-    $.fn.zoom = function($fator,$escala,$seletor,$tag){
-        $(this).click(function(){
-            
-            if( $tag=="xy" ){
-                var sizeW = $($seletor).css("width");
-                var sizeH = $($seletor).css("height");
-                var sizeT = $($seletor).css("top");
-                var sizeL = $($seletor).css("left");
-                
-                var sizeW = sizeW.replace("px","");
-                var sizeH = sizeH.replace("px","");
-                var sizeT = sizeT.replace("px","");
-                var sizeL = sizeL.replace("px","");
+    $.fn.zoom = function($fator,$escala,$seletor){
+        $(this).click(function(){            
+            var size = $($seletor).css(["width","height","top","right","bottom","left","margin-top","margin-right","margin-bottom","margin-left","padding-top","padding-right","padding-bottom","padding-left","font-size"]);
 
+            $.each(size , function(prop,value){            
+                value = value.search("px")>0 ? value.replace("px","") : value ;
+                            
                 if($fator=="*"){
-                    sizeW = Math.round( parseInt(sizeW) * $escala );
-                    sizeH = Math.round( parseInt(sizeH) * $escala );
-                    sizeT = Math.round( parseInt(sizeT) * $escala );
-                    sizeL = Math.round( parseInt(sizeL) * $escala );
+                    value = $.isNumeric(value) ? Math.round( value * $escala ) : value;
                 }else if($fator=="/"){
-                    sizeW = Math.round( parseInt(sizeW) / $escala );
-                    sizeH = Math.round( parseInt(sizeH) / $escala );
-                    sizeT = Math.round( parseInt(sizeT) / $escala );
-                    sizeL = Math.round( parseInt(sizeL) / $escala );
+                    value = $.isNumeric(value) ? Math.round( value / $escala ) : value;
                 }else if($fator=="+"){
-                    sizeW = Math.round( parseInt(sizeW) + $escala );
-                    sizeH = Math.round( parseInt(sizeH) + $escala );
-                    sizeT = Math.round( parseInt(sizeT) + $escala );
-                    sizeL = Math.round( parseInt(sizeL) + $escala );
+                    value = $.isNumeric(value) ? Math.round( value + $escala ) : value;
                 }else if($fator=="-"){
-                    sizeW = Math.round( parseInt(sizeW) - $escala );
-                    sizeH = Math.round( parseInt(sizeH) - $escala );
-                    sizeT = Math.round( parseInt(sizeT) - $escala );
-                    sizeL = Math.round( parseInt(sizeL) - $escala );
-                }
+                    value = $.isNumeric(value) ? Math.round( value - $escala ) : value;
+                }   
 
-                $($seletor).animate({"width":sizeW+"px"});
-                $($seletor).animate({"height":sizeH+"px"});
-                $($seletor).animate({"top":sizeT+"px"});
-                $($seletor).animate({"left":sizeL+"px"});
-            
-            }else{
-                var sizeF = $($seletor).css("font-size");
-                var sizeF = sizeF.replace("px","");
-                sizeF = Math.round( parseInt(sizeF) * $escala );
-                $($seletor).animate({"font-size":size+"px"});
-            }                              
-            
+                if( $.isNumeric(value) ){
+                    switch(prop){
+                        case "width":$($seletor).animate({ "width":value+"px" },100);
+                        break;
+                        case "height":$($seletor).animate({ "height":value+"px" },100);
+                        break;
+                        case "top":$($seletor).animate({ "top":value+"px" },100);
+                        break;
+                        case "right":$($seletor).animate({ "right":value+"px" },100);
+                        break;
+                        case "bottom":$($seletor).animate({ "bottom":value+"px" },100);
+                        break;
+                        case "left":$($seletor).animate({ "left":value+"px" },100);
+                        break;
+                        case "margin-top":$($seletor).animate({ "margin-top":value+"px" },100);
+                        break;
+                        case "margin-right":$($seletor).animate({ "margin-right":value+"px" },100);
+                        break;
+                        case "margin-bottom":$($seletor).animate({ "margin-bottom":value+"px" },100);
+                        break;
+                        case "margin-left":$($seletor).animate({ "margin-left":value+"px" },100);
+                        break;
+                        case "padding-top":$($seletor).animate({ "padding-top":value+"px" },100);
+                        break;
+                        case "padding-right":$($seletor).animate({ "padding-right":value+"px" },100);
+                        break;
+                        case "padding-bottom":$($seletor).animate({ "padding-bottom":value+"px" },100);
+                        break;
+                        case "padding-left":$($seletor).animate({ "padding-left":value+"px" },100);
+                        break;
+                        case "font-size":$($seletor).animate({ "font-size":value+"px" },100);
+                        break;                    
+                    }
+                }
+            });                    
         });
     }
 })(jQuery);
