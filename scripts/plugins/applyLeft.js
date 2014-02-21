8f38f4a441e81af8fef1ebfd5caa54a518731f9c
Filipe Arantes Fernandes <filran@gmail.com>
2014-02-21 10:49:10 -0300

    Layout líquido implementado

--- scripts/plugins/applyLeft.js
+++ scripts/plugins/applyLeft.js
@@ -3,9 +3,9 @@
     $.fn.applyLeft = function( one ){
         first = $(one).css(["width","margin-right","margin-left","padding-right","padding-left"]);
 
-        /*wf = solveWidth(first);
+        wf = solveWidth(first);
 
-        //$( "#"+$(this).attr("id") ).css({ "left":"-"+wf });
+        //$( "#"+$(this).attr("id") ).css({ "left":"-"+wf }); //179-362 = 183 = 102.23%
 
         function solveWidth( object ){
             sum = 0;
@@ -13,10 +13,17 @@
                 value = parseInt(value.replace("px",""));
                 sum+=value;
             });
-            return sum;
-        }*/
+            sum*=2.0223;
+            sum = Math.round(sum);
 
-        sum=0;
+            if(){
+                
+            }
+            
+            alert(sum);
+            return sum;
+        }
+
         $.each(first,function(tag,value){
                 value = parseInt(value.replace("px",""));
                 $("#stake").append(tag+":"+value+"<br>");
--- scripts/plugins/applyTop.js
+++ scripts/plugins/applyTop.js
@@ -9,34 +9,40 @@ one and two = its objects that will to sum height for after solver diference
         
         first = $(one).css(["height","margin-top","margin-bottom","padding-top","padding-bottom"]);
         second = $(two).css(["height","margin-top","margin-bottom","padding-top","padding-bottom"]);
-            
-        //if(id=="obj" || id=="requisitos"){            
-            hf = solveHeight(first);
-            hs = solveHeight(second);
 
-            if( hf < hs ){
-                dif = hf-hs;
-            }else{
-                dif = hs-hf;
-            }
+        hf = solveHeight(first);
+        hs = solveHeight(second);
 
-            $( "#"+$(this).attr("id") ).css({"top":dif});    
+        if( hf < hs ){
+            dif = hf-hs;
+        }else{
+            dif = hs-hf;
+        }
 
-            function solveHeight( whatobject ){
-                sum=0;
-                $.each( whatobject , function( tag , value ){
-                    value = parseInt(value.replace("px","")); 
-                    sum += value;
-                });
-                return sum;
+        if(id=="beneficios"){
+            dif*=1.36;
+            dif = Math.round(dif);
+            if( dif > -195 ){
+                dif-=1;
+                //alert(dif);
             }
-        /*}else if(id=="beneficios"){
-            if( hf > hs ){
-                dif = hf-hs;
-            }else{
-                dif = hs-hf;
-            }
-            //$( "#"+$(this).attr("id") ).css({"top":dif});
-        }*/
+        }else if( id=="custos" ) {
+            dif*=1.107;            
+        }else if( id=="autor" || id=="metodologia"){
+            dif*=1.025;
+        }
+        
+        $( "#"+$(this).attr("id") ).css({"top":dif});
+
+
+        /*just a function*/
+        function solveHeight( whatobject ){
+            sum=0;
+            $.each( whatobject , function( tag , value ){
+                value = parseInt(value.replace("px","")); 
+                sum += value;
+            });
+            return sum;
+        }
     }       
 })(jQuery);
